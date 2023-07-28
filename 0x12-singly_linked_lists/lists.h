1fc54be75be8b65de6b1e4e42297f0528c8be7a8
Josinimasango <nellyjossydumako@gmail.com>
2023-07-28 16:00:26 -0700

    adding print_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,18 +2,19 @@
 #define LISTS_H
 
 /**
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- *singly linked list node structure
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} 
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
