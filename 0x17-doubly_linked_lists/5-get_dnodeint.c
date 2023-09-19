ec118fdebf344d79b66000bd69eb47b28cc9bb35
Josinimasango <nellyjossydumako@gmail.com>
2023-09-19 10:59:05 -0700

    get node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,26 +1,26 @@
+/*
+ * File: 5-get_dnodeint.c
+ * Auth: Nelisiwe Masango
+ */
+
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node
- * @head: double pointer to the head
- * @index: index of the node to return
+ * get_dnodeint_at_index - locates a node
+ * @head: the head pointer
+ * @index: node to be located
  *
- * Return: if the node does not exist, return NULL
+ * Return: If there is no node return NULL.
+ *         Otherwise - the address of the located node.
  */
-
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
-
-	current = head;
-	i = 0;
-	while (current != NULL)
+	for (; index != 0; index--)
 	{
-		if (i == index)
-			return (current);
-		current = current->next;
-		i++;
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
 	}
-	return (NULL);
+
+	return (head);
 }
